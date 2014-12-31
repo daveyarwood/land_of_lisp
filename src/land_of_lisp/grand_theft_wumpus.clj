7bf4ff256c1319feb0d396292f6bd36a933b1ffd
Dave Yarwood <dave.yarwood@gmail.com>
2014-12-31 14:56:05 -0500

    still working on chapter 8

--- src/land_of_lisp/grand_theft_wumpus.clj
+++ src/land_of_lisp/grand_theft_wumpus.clj
@@ -1,6 +1,6 @@
 (ns land-of-lisp.grand-theft-wumpus
   (require [land-of-lisp.graph-util :refer :all]
-           [clojure.set :refer (difference)]))
+           [clojure.set :refer (difference intersection)]))
 
 (def congestion-city-nodes (ref nil))
 (def congestion-city-edges (ref nil))
@@ -8,7 +8,7 @@
 (def node-num (ref 30))
 (def edge-num (ref 45))
 (def worm-num (ref 3))
-(def cop-odds (ref 15))
+(def cop-odds (ref 1/15))
 
 ;; generating random edges
 
@@ -29,7 +29,7 @@
   (filter #(= (first %) node) edge-list))
 
 (defn get-connected
-  "Returns the set of all nodes connected to a given node."
+  "Returns the set of all nodes reachable from a given node."
   [node edge-list]
   (let [visited (atom #{})
         traverse (fn traverse [node]
@@ -58,3 +58,34 @@
   (let [islands (find-islands nodes edge-list)
         new-connections (mapcat #(connect-island % edge-list) islands)]
     (concat edge-list new-connections)))
+
+(defn edge-map
+  "Returns a map of each node to the nodes to which it is connected."
+  [edge-list]
+  (into {}
+    (for [node (range 1 (inc @edge-num))
+          :let [direct-edges (direct-edges node edge-list)]
+          :when (not (empty? direct-edges))]
+      [node (set (map second direct-edges))])))
+
+(defn add-cops
+  "Returns a 'nested alist' of each node to the nodes to which it is connected;
+   if the edge contains cops, that edge is associated with the symbol 'cops.
+
+   Using this format to follow suit with the book, which uses this format in
+   order to be compatible with the graphing libraries."
+  [edge-map cops]
+  (map (fn [[node edges]]
+         (cons node
+               (for [edge edges]
+                 (if-not (empty? (intersection (set cops)
+                                               (set (edge-pair node edge))))
+                   (list edge 'cops)
+                   (list edge)))))
+       edge-map))
+
+(defn make-city-edges []
+  (let [nodes (range 1 (inc @edge-num))
+        edge-list (connect-all-islands nodes (make-edge-list))
+        cops (filter (fn [_] (<= (rand) @cop-odds)) edge-list)]
+    (add-cops (edge-map edge-list) cops)))
