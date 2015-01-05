cd7edcac818297f5d94d3406c15d639a55833382
Dave Yarwood <dave.yarwood@gmail.com>
2015-01-04 21:49:49 -0500

    finish chapter 8

--- src/land_of_lisp/grand_theft_wumpus.clj
+++ src/land_of_lisp/grand_theft_wumpus.clj
@@ -48,8 +48,8 @@
   (difference (set nodes) (connected-nodes edge-list)))
 
 (defn connect-island
-  "Returns new edges going to and from an island and a randomly chosen node that is
-   already connected."
+  "Returns new edges going to and from an island and a randomly chosen node
+   that is already connected."
   [island edge-list]
   (edge-pair island (rand-nth (seq (connected-nodes edge-list)))))
 
@@ -78,80 +78,41 @@
                 (list edge 'cops)
                 (list edge)))])))
 
-(defn add-cops2 ;; alist version
-  "Returns a 'nested alist' of each node to the nodes to which it is connected;
-   if the edge contains cops, that edge is associated with the symbol 'cops.
-
-   Using this format to follow suit with the book, which uses this format in
-   order to be compatible with the graphing libraries."
-  [edge-map cops]
-  (map (fn [[node edges]]
-         (cons node
-               (for [edge edges]
-                 (if-not (empty? (intersection (set cops)
-                                               (set (edge-pair node edge))))
-                   (list edge 'cops)
-                   (list edge)))))
-       edge-map))
-
 (defn make-city-edges []
-  (let [nodes (range 1 (inc *edge-num*))
+  (let [nodes (range 1 (inc *node-num*))
         edge-list (connect-all-islands nodes (make-edge-list))
         cops (filter (fn [_] (<= (rand) *cop-odds*)) edge-list)]
     (add-cops (edge-map edge-list) cops)))
 
 ;; building the nodes
 
-(defn look-up-node [node edge-alist]
-  "Utility function for retrieving a node's entry from an alist.
-   (In CL, this is just 'assoc'.)"
-  (first (filter #(= (first %) node) edge-alist)))
-
 (defn neighbors [node edge-map]
-  (let [[nexus & neighbors] (edge-map node)]
-    (map first neighbors)))
+  (map first (edge-map node)))
 
-(defn neighbors2 [node edge-alist] ;; alist version
-  (let [[nexus & neighbors] (look-up-node node edge-alist)]
-    (map first neighbors)))
+(defn within-one? [a b edge-map]
+  (contains? (set (neighbors a edge-map)) b))
 
-(defn within-one? [a b edge-alist]
-  (contains? (set (neighbors a edge-alist)) b))
-
-(defn within-two?  [a b edge-alist]
+(defn within-two?  [a b edge-map]
   (boolean
-    (or (within-one? a b edge-alist)
-        (some #(within-one? % b edge-alist) (neighbors a edge-alist)))))
+    (or (within-one? a b edge-map)
+        (some #(within-one? % b edge-map) (neighbors a edge-map)))))
 
 (defn make-city-nodes [edge-map]
   (let [wumpus (random-node)
         glow-worms (repeatedly *worm-num* #(random-node))]
-    (for [n (range 1 (inc *node-num*))]
-      (remove nil?
+    (into {}
+      (for [n (range 1 (inc *node-num*))]
         [n
-         (cond
-           (= n wumpus) 'wumpus
-           (within-two? n wumpus edge-map) 'blood!)
-         (cond
-           ((set glow-worms) n) 'glow-worm
-           (some #(within-one? n % edge-map) glow-worms) 'lights!)
-         (let [[node & neighbors] (edge-map n)]
-           (when (some #(= (second %) 'cops) neighbors) 'sirens!))]))))
-
-(defn make-city-nodes2 [edge-alist] ;; alist version
-  (let [wumpus (random-node)
-        glow-worms (repeatedly *worm-num* #(random-node))]
-    (for [n (range 1 (inc *node-num*))]
-      (remove nil?
-        [n
-         (cond
-           (= n wumpus) 'wumpus
-           (within-two? n wumpus edge-alist) 'blood!)
-         (cond
-           ((set glow-worms) n) 'glow-worm
-           (some #(within-one? n % edge-alist) glow-worms) 'lights!)
-         (let [[node & neighbors] (look-up-node n edge-alist)]
-           (when (some #(= (second %) 'cops) neighbors) 'sirens!))]))))
+        (remove nil?
+          (list
+           (cond
+             (= n wumpus) 'wumpus
+             (within-two? n wumpus edge-map) 'blood!)
+           (cond
+             ((set glow-worms) n) 'glow-worm
+             (some #(within-one? n % edge-map) glow-worms) 'lights!)
+           (let [[node & neighbors] (edge-map n)]
+             (when (some #((set %) 'cops) neighbors) 'sirens!))))]))))
 
 ;; game state
 
@@ -160,20 +121,89 @@
 (def player-pos (ref nil))
 (def visited-nodes (ref nil))
 
+;; drawing only known city nodes
+
+(defn known-city-nodes []
+  (let [visited-info (zipmap @visited-nodes
+                             (map @congestion-city-nodes @visited-nodes))
+        neighbors (set (mapcat #(neighbors % @congestion-city-edges)
+                               @visited-nodes))
+        mystery-neighbors (into {} (map #(vector % '(?)) neighbors))]
+    (merge mystery-neighbors
+           visited-info
+           {@player-pos (conj (visited-info @player-pos) '*)})))
+
+(defn known-city-edges []
+  (let [visited-info (zipmap @visited-nodes
+                             (map @congestion-city-edges @visited-nodes))]
+    (into {}
+      (for [[node edges] visited-info]
+        [node (for [edge edges]
+                (if ((set @visited-nodes) (first edge))
+                  edge
+                  (list (first edge))))]))))
+
+(defn draw-city! []
+  (ugraph->png "city.dot" (for [[node stuff-in-it] @congestion-city-nodes]
+                            (cons node stuff-in-it))
+                          @congestion-city-edges))
+
+(defn draw-known-city! []
+  (ugraph->png "known-city.dot" (for [[node stuff-in-it] (known-city-nodes)]
+                              (cons node stuff-in-it))
+                            (known-city-edges)))
+
 ;; initializing a new game
 
 (defn find-empty-node []
-  (let [x (random-node)
-        [node & stuff-in-it] (look-up-node x @congestion-city-nodes)]
-    (if stuff-in-it (recur) x)))
+  (let [x (random-node)]
+    (if (empty? (@congestion-city-nodes x))
+      x
+      (recur))))
 
-(defn draw-city []
-  (ugraph->png "city.dot" @congestion-city-nodes @congestion-city-edges))
-
-(defn new-game []
+(defn new-game! []
   (dosync
     (ref-set congestion-city-edges (make-city-edges))
     (ref-set congestion-city-nodes (make-city-nodes @congestion-city-edges))
     (ref-set player-pos (find-empty-node))
-    (ref-set visited-nodes [@player-pos]))
-  (draw-city))
+    (ref-set visited-nodes (list @player-pos)))
+  (draw-city!)
+  (draw-known-city!))
+
+;; walking around town
+
+(declare handle-direction handle-new-place)
+
+(defn walk [pos]
+  (handle-direction pos))
+
+(defn charge [pos]
+  (handle-direction pos :charging true))
+
+(defn handle-direction [pos & {:keys [charging]}]
+  (let [possible-edges (set (neighbors @player-pos @congestion-city-edges))]
+    (if (contains? possible-edges pos)
+      (handle-new-place pos :charging charging)
+      (println "You can't get there from where you are!"))))
+
+(defn handle-new-place [pos & {:keys [charging]}]
+  (let [node (@congestion-city-nodes pos)
+        glow-worm? (and ((set node) 'glow-worm)
+                        (not ((set @visited-nodes) pos)))
+        cops? (some #(and ((set %) pos) ((set %) 'cops))
+                    (@congestion-city-edges @player-pos))
+        wumpus? ((set node) 'wumpus)]
+    (dosync
+      (commute visited-nodes conj pos)
+      (ref-set player-pos pos))
+    (draw-known-city!)
+    (cond
+      cops? (println "You ran into the cops. Game over.")
+      wumpus? (if charging
+                (println "You found the wumpus!")
+                (println "You ran into the wumpus. Game over."))
+      charging (println "You wasted your last bullet. Game over.")
+      glow-worm? (let [new-pos (random-node)]
+                   (println "You ran into a Glow Worm Gang!"
+                            "You are now at" new-pos)
+                   (handle-new-place new-pos)))))
